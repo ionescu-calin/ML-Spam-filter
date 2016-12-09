4d7b6b3e94d61c2485a9085e32d1271aca794b96
Silviu Tugulan <t.silviu94@gmail.com>
2016-12-09 19:22:15 +0000

    Tfidf implemented
    It seems to work very bad on the spam

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.cross_validation import StratifiedKFold
 from sklearn.cross_validation import KFold
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
 from sklearn.metrics import f1_score, confusion_matrix, accuracy_score, recall_score
 from sklearn.model_selection import cross_val_score
 from sklearn.externals import joblib
@@ -22,9 +22,12 @@ def evaluate_classifier_kf(data, kfold, clf):
 	    test_y = data.iloc[test_indices]['class'].values
 
 	    features_counts = count_vectorizer.fit_transform(train_text)
+	    # features_counts_tfidf = tfidf.fit_transform(features_counts)
+	    # print features_counts
 	    clf.fit(features_counts, train_y)
 
 	    test_counts = count_vectorizer.transform(test_text)
+	    # test_counts_tfidf = tfidf.transform(test_counts)
 	    predictions = clf.predict(test_counts)
 
 	    confusion += confusion_matrix(test_y, predictions)
@@ -44,7 +47,8 @@ def evaluate_classifier_kf(data, kfold, clf):
 def train_classifier(data, clf_trained):
 	train_text = data['text'].values
 	features_counts = count_vectorizer.fit_transform(train_text)
-	clf_trained.fit(features_counts, data['class'].values)
+	# features_counts_tfidf = tfidf.fit_transform(features_counts)
+	# clf_trained.fit(features_counts_tfidf, data['class'].values)
 
 	dictionay_filepath = "trained_vocab.pkl"
 	joblib.dump(count_vectorizer, dictionay_filepath)
@@ -56,20 +60,21 @@ filename = 'out.csv'
 # Load data from csv file
 data = pd.DataFrame.from_csv(filename)
 
-# Init count vectorizer
-# count_vectorizer = CountVectorizer()
-
-# Init count vectorizer with ngrams
-count_vectorizer = CountVectorizer(
+# Init count vectorizer with params
+count_vectorizer = TfidfVectorizer(
 	analyzer='word',
 	lowercase=False, 
 	#preprocessor=None,
 	#tokenizer=None,
-	ngram_range=(1, 3)
+	ngram_range=(1, 3),
+	min_df=1
 	)
 	#df=[0.7, 1.0], 
 	# token_pattern=r'\b\w+\b', 
 	#min_df=1)
+
+# Init tfidf transformer
+#tfidf = TfidfTransformer(smooth_idf=True)
 
 # Randomize data indices
 data = data.reindex(np.random.permutation(data.index))
@@ -83,10 +88,10 @@ skf = StratifiedKFold(y, n_folds=10)
 
 # Init Baseline classifier (Random or based on the most likely class distribution)
 # strategy="most_frequent" if the class distribution is not balanced
-clf = DummyClassifier(strategy="uniform")
+# clf = DummyClassifier(strategy="uniform")
 
 # Evaluate dummy 
-evaluate_classifier_kf(data, skf, clf)
+# evaluate_classifier_kf(data, skf, clf)
 
 # Init classifier Multinomial classifier
 clf = MultinomialNB()
